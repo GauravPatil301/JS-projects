affda1424e74710ba1c15476f5fc1377ec080520
Gaurav Patil <301gaurav@gmail.com>
2023-04-08 10:28:05 +0530

    grocery bud part 1 slider project intro

---
+++ Grocery-bud/app.js
@@ -0,0 +1,36 @@
+// *************** Select Items ***************
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list')
+const clearBtn = document.querySelector('.clear-btn')
+
+// edit option
+let editElement;
+let editFlag = false;
+let editID = "";
+
+
+// *************** Event Listeners *************
+// Submit form
+form.addEventListener("submit",addItem);
+
+// *************** Functions *******************
+function addItem(e) {
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+    if(value !== '' && editFlag === false){
+
+    }else if(value !== '' && editFlagg === true){
+
+    }else{
+        console.log("empty value");
+    }
+}
+// *************** Local Storage ***************
+
+
+// *************** Setup Items *****************
